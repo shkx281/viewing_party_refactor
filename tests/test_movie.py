1d93b8bf1502e32973159029554a72eff61a6c54
Kelly Kang <shkx281@gmail.com>
2022-04-01 10:35:52 -0700

    Finished activity

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -1,9 +1,28 @@
 import pytest
+from viewing_party.movie import Movie
 
-def test_1():
+def test_new_valid_movie():
     # Arrange
+    title = "Star Wars"
+    genre = "Sci-Fi"
+    rating = 5
+    host = "Netflix"
 
     # Act
 
+    star_wars = Movie(title=title, genre=genre, rating=rating, host=host)
+
     # Assert
-    pass
+
+    assert star_wars.title == title
+    assert star_wars.genre == genre
+    assert star_wars.rating == rating
+    assert star_wars.host == host
+
+def test_update_movie_rating():
+    # Arrange
+    star_wars = Movie(title="Star Wars", genre="Sci-fi", rating=5, host = "Netflix")
+    # Act
+    star_wars.update_rating(4)
+    # Assert
+    assert star_wars.rating == 4
--- tests/test_person.py
+++ tests/test_person.py
@@ -1,9 +1,48 @@
 import pytest
+from viewing_party.person import Person
 
-def test_1():
+def test_new_valid_person():
     # Arrange
-
+    name = "Bob"
+    watched = ["Star Wars", "Spongebob"]
+    friends = ["Mandy", "Nish"]
+    subscriptions = ["Hulu", "DisneyPlus", "HBOMax", "Netflix"]
     # Act
 
+    bob = Person(name=name, watched=watched, friends=friends, subscriptions=subscriptions)
+
     # Assert
-    pass
+
+    assert bob.name == name
+    assert bob.watched == watched
+    assert bob.friends == friends
+    assert bob.subscriptions == subscriptions
+
+def test_add_watched_movie():
+    # Arrange
+    name = "Bob"
+    watched = ["Star Wars", "Spongebob"]
+    friends = ["Mandy", "Nish"]
+    subscriptions = ["Hulu", "DisneyPlus", "HBOMax", "Netflix"]
+
+    bob = Person(name=name, watched=watched, friends=friends, subscriptions=subscriptions)
+
+    # Act
+    bob.add_watched_movie("Shrek")
+
+    # Assert
+    assert bob.watched == ["Star Wars", "Spongebob", "Shrek"]
+
+def test_num_of_movies_watched():
+    # Arrange
+    name = "Bob"
+    watched = ["Star Wars", "Spongebob"]
+    friends = ["Mandy", "Nish"]
+    subscriptions = ["Hulu", "DisneyPlus", "HBOMax", "Netflix"]
+
+    bob = Person(name=name, watched=watched, friends=friends, subscriptions=subscriptions)
+    # Assert
+
+    assert bob.get_num_watched() == 2
+
+    
